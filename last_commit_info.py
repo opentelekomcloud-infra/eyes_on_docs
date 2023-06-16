cf75986bec1e7b43309b63a896cb0b8c249922d5
YustinaKvr <62885041+YustinaKvr@users.noreply.github.com>
2023-06-16 10:24:58 +0200

    Dashboard updates (#11)
    Dashboard updates
    
    Error handling for all the scripts
    
    Reviewed-by: Vladimir Vshivkov

--- last_commit_info.py
+++ last_commit_info.py
@@ -5,6 +5,11 @@ import psycopg2
 from git import Repo
 from github import Github
 from datetime import datetime
+import time
+
+start_time = time.time()
+
+print("**LAST COMMIT INFO SCRIPT IS RUNNING**")
 
 github_token = os.getenv("GITHUB_TOKEN")
 g = Github(github_token)
@@ -18,44 +23,64 @@ db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
 
 
+def check_env_variables():
+    required_env_vars = [
+        "GITHUB_TOKEN", "DB_HOST", "DB_PORT",
+        "DB_NAME", "DB_USER", "DB_PASSWORD", "GITEA_TOKEN"
+    ]
+    for var in required_env_vars:
+        if os.getenv(var) is None:
+            raise Exception(f"Missing environment variable: {var}")
+
+
 def connect_to_db():
-    return psycopg2.connect(
-        host=db_host,
-        port=db_port,
-        dbname=db_name,
-        user=db_user,
-        password=db_password
-    )
+    print("Connecting to Postgres...")
+    try:
+        return psycopg2.connect(
+            host=db_host,
+            port=db_port,
+            dbname=db_name,
+            user=db_user,
+            password=db_password
+        )
+    except psycopg2.Error as e:
+        print(f"Connecting to Postgres: an error occurred while trying to connect to the database: {e}")
+        return None
 
 
 def create_commits_table(conn, cur, table_name):
-    cur.execute(
-        f'''CREATE TABLE IF NOT EXISTS {table_name} (
-        id SERIAL PRIMARY KEY,
-        "Service Name" VARCHAR(255),
-        "Doc Type" VARCHAR(255),
-        "Squad" VARCHAR(255),
-        "Last commit at" VARCHAR(255),
-        "Days passed" INT,
-        "Commit URL" VARCHAR(255)
-        );'''
-    )
-    conn.commit()
+    try:
+        cur.execute(
+            f'''CREATE TABLE IF NOT EXISTS {table_name} (
+            id SERIAL PRIMARY KEY,
+            "Service Name" VARCHAR(255),
+            "Doc Type" VARCHAR(255),
+            "Squad" VARCHAR(255),
+            "Last commit at" VARCHAR(255),
+            "Days passed" INT,
+            "Commit URL" VARCHAR(255)
+            );'''
+        )
+        conn.commit()
+        print(f"Table {table_name} has been created successfully")
+    except psycopg2.Error as e:
+        print(f"Tables creating: an error occurred while trying to create a table {table_name} in the database: {e}")
 
 
 def get_last_commit_url(github_repo, git_repo, path):
-    last_commit_sha = git_repo.git.log('-1', '--pretty=format:%H', path)
-    last_commit_url = f"https://github.com/{github_repo.full_name}/commit/{last_commit_sha}"
-    return last_commit_url
-
+    try:
+        last_commit_sha = git_repo.git.log('-1', '--pretty=format:%H', path)
+        last_commit_url = f"https://github.com/{github_repo.full_name}/commit/{last_commit_sha}"
+        return last_commit_url
+    except Github.GithubException.GithubException as e:
+        print(f"SHA: an error occurred while getting last commit URL: {e}")
 
 def get_last_commit(cur, conn, doctype):
+    print("Gathering last commit info...")
     exclude_repos = ["docsportal", "doc-exports", "docs_on_docs", ".github", "presentations", "sandbox", "security", "template"]
     for repo in org.get_repos():
         if repo.name in exclude_repos:
             continue
-
-        print(f"Processing {repo.name}...")
 
         tmp_dir = tempfile.mkdtemp()
 
@@ -70,7 +95,6 @@ def get_last_commit(cur, conn, doctype):
                     now = datetime.utcnow()
                     duration = now - last_commit
                     duration_days = duration.days
-                    print(f"Last commit time in {path}: {last_commit}")
                     if doctype == "umn/source":
                         doc_type = "UMN"
                     else:
@@ -81,51 +105,61 @@ def get_last_commit(cur, conn, doctype):
                         (service_name, doc_type, last_commit_str, duration_days, last_commit_url,))
                     conn.commit()
                 except Exception as e:
-                    print(f"Error while running git log for path {path}: {str(e)}")
+                    print(f"Last commit: an error occurred while running git log for path {path}: {str(e)}")
 
         except Exception as e:
-            print(f"Error while processing repo {repo.name}: {str(e)}")
+            print(f"Last commit: an error occurred while processing repo {repo.name}: {str(e)}")
 
         finally:
             shutil.rmtree(tmp_dir)
 
 
-def update_service_names_and_squad(cur, conn):
-    cur.execute("SELECT id, \"Service Name\" FROM last_update_commit;")
-    last_update_commit_rows = cur.fetchall()
+def update_squad_and_title(conn, cur):
+    print("Updating squads and titles...")
+    try:
+        cur.execute("SELECT * FROM last_update_commit;")
+        open_issues_rows = cur.fetchall()
 
-    cur.execute("SELECT \"Repository\", \"Title\", \"Category\" FROM repo_title_category;")
-    repo_title_category_rows = cur.fetchall()
-
-    repo_dict = {repo: (title, category) for repo, title, category in repo_title_category_rows}
-
-    for row_id, service_name in last_update_commit_rows:
-        if service_name in repo_dict:
-            pretty_service_name, squad = repo_dict[service_name]
-
+        for row in open_issues_rows:
             cur.execute(
-                'UPDATE last_update_commit SET "Service Name" = %s, "Squad" = %s WHERE id = %s;',
-                (pretty_service_name, squad, row_id))
+                """UPDATE last_update_commit
+                    SET "Service Name" = rtc."Title", "Squad" = rtc."Category"
+                    FROM repo_title_category AS rtc
+                    WHERE last_update_commit."Service Name" = rtc."Repository"
+                    AND last_update_commit.id = %s;""",
+                (row[0],)
+            )
+            cur.execute(
+                """UPDATE last_update_commit
+                    SET "Squad" = 'Other'
+                    WHERE last_update_commit."Service Name" IN ('doc-exports', 'docs_on_docs', 'docsportal')
+                    AND last_update_commit.id = %s;""",
+                (row[0],)
+            )
             conn.commit()
-    print("Update completed.")
 
-
-
-def fetch_repo_title_category(cur):
-    cur.execute("SELECT * FROM repo_title_category")
-    return cur.fetchall()
+    except Exception as e:
+        print(f"Error updating squad and title: {e}")
+        conn.rollback()
 
 
 def main():
+    check_env_variables()
 
     conn = connect_to_db()
     cur = conn.cursor()
     cur.execute("DROP TABLE IF EXISTS last_update_commit")
     create_commits_table(conn, cur, "last_update_commit")
+    print(f"Searching for a most recent commit in umn/source...")
     get_last_commit(cur, conn, "umn/source")
+    print(f"Searching for a most recent commit in api-ref/source...")
     get_last_commit(cur, conn, "api-ref/source")
-    update_service_names_and_squad(cur, conn)
+    update_squad_and_title(conn, cur)
     conn.commit()
+    end_time = time.time()
+    execution_time = end_time - start_time
+    minutes, seconds = divmod(execution_time, 60)
+    print(f"Script executed in {int(minutes)} minutes {int(seconds)} seconds! Let's go drink some beer :)")
 
 
 if __name__ == "__main__":
--- open_issues.py
+++ open_issues.py
@@ -5,6 +5,11 @@ import psycopg2
 from datetime import datetime
 from github import Github
 import requests
+import time
+
+start_time = time.time()
+
+print("**OPEN ISSUES SCRIPT IS RUNNING**")
 
 gitea_api_endpoint = "https://gitea.eco.tsi-dev.otc-service.com/api/v1"
 session = requests.Session()
@@ -23,150 +28,202 @@ db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
 
 
+def check_env_variables():
+    required_env_vars = [
+        "GITHUB_TOKEN", "DB_HOST", "DB_PORT",
+        "DB_NAME", "DB_USER", "DB_PASSWORD", "GITEA_TOKEN"
+    ]
+    for var in required_env_vars:
+        if os.getenv(var) is None:
+            raise Exception(f"Missing environment variable: {var}")
+
+
 def connect_to_db():
-    return psycopg2.connect(
-        host=db_host,
-        port=db_port,
-        dbname=db_name,
-        user=db_user,
-        password=db_password
-    )
+    print("Connecting to Postgres...")
+    try:
+        return psycopg2.connect(
+            host=db_host,
+            port=db_port,
+            dbname=db_name,
+            user=db_user,
+            password=db_password
+        )
+    except psycopg2.Error as e:
+        print(f"Connecting to Postgres: an error occurred while trying to connect to the database: {e}")
+        return None
 
 
 def create_open_issues_table(conn, cur):
-    cur.execute(
-        '''CREATE TABLE IF NOT EXISTS open_issues (
-        id SERIAL PRIMARY KEY,
-        "Environment" VARCHAR(255),
-        "Service Name" VARCHAR(255),
-        "Squad" VARCHAR(255),
-        "Issue Number" INT,
-        "Issue Title" VARCHAR(255),
-        "Created by" VARCHAR(255),
-        "Created at" TIMESTAMP,
-        "Duration" INT,
-        "Comments" INT,
-        "Assignees" TEXT,
-        "Issue URL" VARCHAR(255)
-        );'''
-    )
-    conn.commit()
+    try:
+        cur.execute(
+            '''CREATE TABLE IF NOT EXISTS open_issues (
+            id SERIAL PRIMARY KEY,
+            "Environment" VARCHAR(255),
+            "Service Name" VARCHAR(255),
+            "Squad" VARCHAR(255),
+            "Issue Number" INT,
+            "Issue URL" VARCHAR(255),
+            "Created by" VARCHAR(255),
+            "Created at" TIMESTAMP,
+            "Duration" INT,
+            "Comments" INT,
+            "Assignees" TEXT
+            );'''
+        )
+        conn.commit()
+        print(f"Table open_issues has been created successfully")
+    except psycopg2.Error as e:
+        print(f"Tables creating: an error occurred while trying to create a table open_issues in the database: {e}")
 
 
 def get_gitea_issues(gitea_token):
+    print("Gathering Gitea issues...")
     gitea_issues = []
     page = 1
     while True:
-        repos_resp = requests.get(f"{gitea_api_endpoint}/repos/issues/search?state=open&owner={gitea_org}&page={page}&limit=1000&token={gitea_token}")
-        if repos_resp.status_code == 200:
+        try:
+            repos_resp = requests.get(f"{gitea_api_endpoint}/repos/issues/search?state=open&owner={gitea_org}&page={page}&limit=1000&token={gitea_token}")
+            repos_resp.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print(f"Gitea issues: an error occurred while trying to get Gitea issues: {e}")
+            break
+
+        try:
             issues_dict = json.loads(repos_resp.content.decode())
             for issue in issues_dict:
                 gitea_issues.append(issue)
-            link_header = repos_resp.headers.get("Link")
-            if link_header is None or "rel=\"next\"" not in link_header:
-                break
-            else:
-                page += 1
-        else:
+        except json.JSONDecodeError as e:
+            print(f"Gitea issues: an error occurred while trying to decode JSON: {e}")
             break
+
+        link_header = repos_resp.headers.get("Link")
+        if link_header is None or "rel=\"next\"" not in link_header:
+            break
+        else:
+            page += 1
+
     return gitea_issues
 
 
 def get_github_issues(github_token, repo_names):
+    print("Gathering Github issues...")
     headers = {"Authorization": f"Bearer {github_token}"}
     github_issues = []
     for repo in repo_names:
-        url = f"https://api.github.com/repos/{github_org}/{repo}/issues"
-        params = {"state": "open"}
-        repos_resp = requests.get(url, headers=headers, params=params)
-        print(repos_resp.status_code)
-        if repos_resp.status_code == 200:
+        try:
+            url = f"https://api.github.com/repos/{github_org}/{repo}/issues"
+            params = {"state": "open"}
+            repos_resp = requests.get(url, headers=headers, params=params)
+            repos_resp.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print(f"Github issues: an error occurred while trying to get Github issues for repo {repo}: {e}")
+            continue
+
+        try:
             issues_dict = json.loads(repos_resp.content.decode())
             github_issues.extend(issues_dict)
+        except json.JSONDecodeError as e:
+            print(f"Github issues: an error occurred while trying to decode JSON: {e}")
+            continue
+
     return github_issues
 
 
 def get_issues_table(gitea_issues, github_issues, cur, conn):
-    for tea in gitea_issues:
-        environment = "Gitea"
-        service_name = tea['repository']['name']
-        squad = ""
-        number = tea['number']
-        title = tea['title']
-        url = tea['html_url']
-        if "pulls" in url:
-            continue
-        else:
+    print("Posting data to Postgres...")
+    try:
+        for tea in gitea_issues:
+            environment = "Gitea"
+            service_name = tea['repository']['name']
+            squad = ""
+            number = tea['number']
             url = tea['html_url']
-        user = tea['user']['full_name']
-        if user == "":
-            user = "proposalbot"
-        else:
+            if "pulls" in url:
+                continue
+            else:
+                url = tea['html_url']
             user = tea['user']['full_name']
-        created_at = datetime.strptime(tea['created_at'], '%Y-%m-%dT%H:%M:%SZ')
-        now = datetime.utcnow()
-        duration = now - created_at
-        duration_days = duration.days
-        comments = tea['comments']
-        if 'assignees' in tea and tea['assignees'] is not None:
-            assignees = ', '.join([assignee['login'] for assignee in tea['assignees']])
-        else:
-            assignees = ''
-        cur.execute('INSERT INTO open_issues ("Environment", "Service Name", "Squad", "Issue Number", "Issue Title", "Created by", "Created at", "Duration", "Comments", "Assignees", "Issue URL") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
-                    (environment, service_name, squad, number, title, user, created_at, duration_days, comments, assignees, url))
-        conn.commit()
+            if user == "":
+                user = "proposalbot"
+            else:
+                user = tea['user']['full_name']
+            created_at = datetime.strptime(tea['created_at'], '%Y-%m-%dT%H:%M:%SZ')
+            now = datetime.utcnow()
+            duration = now - created_at
+            duration_days = duration.days
+            comments = tea['comments']
+            if 'assignees' in tea and tea['assignees'] is not None:
+                assignees = ', '.join([assignee['login'] for assignee in tea['assignees']])
+            else:
+                assignees = ''
+            cur.execute('INSERT INTO open_issues ("Environment", "Service Name", "Squad", "Issue Number", "Issue URL", "Created by", "Created at", "Duration", "Comments", "Assignees") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
+                        (environment, service_name, squad, number, url, user, created_at, duration_days, comments, assignees))
+            conn.commit()
+    except Exception as e:
+        print(f"Issues table: an error occurred while posting data to Postgres: {e}")
+        conn.rollback()
 
     service_pattern = re.compile(r"(?P<service_name>(?<=\/opentelekomcloud-docs\/)([^\/]+)(?=\/))")
-    for hub in github_issues:
-        if hub:
-            environment = "Github"
-            service_match = None
-            for gh in service_pattern.finditer(hub['url']):
-                service_match = gh
-                break
-            if service_match is not None:
-                service_name = service_match.group("service_name").strip()
-                squad = ""
-                number = hub['number']
-                title = hub['title']
-                url = hub['html_url']
-                user = hub['user']['login']
-                created_at = datetime.strptime(hub['created_at'], '%Y-%m-%dT%H:%M:%SZ')
-                now = datetime.utcnow()
-                duration = now - created_at
-                duration_days = duration.days
-                comments = hub['comments']
-                assignees = ', '.join([assignee['login'] for assignee in hub['assignees']])
+    try:
+        for hub in github_issues:
+            if hub:
+                environment = "Github"
+                service_match = None
+                for gh in service_pattern.finditer(hub['url']):
+                    service_match = gh
+                    break
+                if service_match is not None:
+                    service_name = service_match.group("service_name").strip()
+                    squad = ""
+                    number = hub['number']
+                    url = hub['html_url']
+                    user = hub['user']['login']
+                    created_at = datetime.strptime(hub['created_at'], '%Y-%m-%dT%H:%M:%SZ')
+                    now = datetime.utcnow()
+                    duration = now - created_at
+                    duration_days = duration.days
+                    comments = hub['comments']
+                    assignees = ', '.join([assignee['login'] for assignee in hub['assignees']])
 
-                cur.execute('INSERT INTO open_issues ("Environment", "Service Name", "Squad", "Issue Number", "Issue Title", "Created by", "Created at", "Duration", "Comments", "Assignees", "Issue URL") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
-                            (environment, service_name, squad, number, title, user, created_at, duration_days, comments, assignees, url))
-                conn.commit()
+                    cur.execute('INSERT INTO open_issues ("Environment", "Service Name", "Squad", "Issue Number", "Issue URL", "Created by", "Created at", "Duration", "Comments", "Assignees") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
+                                (environment, service_name, squad, number, url, user, created_at, duration_days, comments, assignees))
+                    conn.commit()
+    except Exception as e:
+        print(f"Issues table: an error occurred while posting data to Postgres: {e}")
+        conn.rollback()
 
 
 def update_squad_and_title(conn, cur):
-    cur.execute("SELECT * FROM open_issues;")
-    open_issues_rows = cur.fetchall()
+    print("Updating squads and titles...")
+    try:
+        cur.execute("SELECT * FROM open_issues;")
+        open_issues_rows = cur.fetchall()
 
-    for row in open_issues_rows:
-        cur.execute(
-            """UPDATE open_issues
-                SET "Service Name" = rtc."Title", "Squad" = rtc."Category"
-                FROM repo_title_category AS rtc
-                WHERE open_issues."Service Name" = rtc."Repository"
-                AND open_issues.id = %s;""",
-            (row[0],)
-        )
-        cur.execute(
-            """UPDATE open_issues
-                SET "Squad" = 'Other'
-                WHERE open_issues."Service Name" IN ('doc-exports', 'docs_on_docs', 'docsportal')
-                AND open_issues.id = %s;""",
-            (row[0],)
-        )
-        conn.commit()
+        for row in open_issues_rows:
+            cur.execute(
+                """UPDATE open_issues
+                    SET "Service Name" = rtc."Title", "Squad" = rtc."Category"
+                    FROM repo_title_category AS rtc
+                    WHERE open_issues."Service Name" = rtc."Repository"
+                    AND open_issues.id = %s;""",
+                (row[0],)
+            )
+            cur.execute(
+                """UPDATE open_issues
+                    SET "Squad" = 'Other'
+                    WHERE open_issues."Service Name" IN ('doc-exports', 'docs_on_docs', 'docsportal')
+                    AND open_issues.id = %s;""",
+                (row[0],)
+            )
+            conn.commit()
+
+    except Exception as e:
+        print(f"Error updating squad and title: {e}")
+        conn.rollback()
 
 
 def main():
+    check_env_variables()
     g = Github(github_token)
     org = g.get_organization("opentelekomcloud-docs")
     repo_names = [repo.name for repo in org.get_repos()]
@@ -186,6 +243,11 @@ def main():
     get_issues_table(gitea_issues, github_issues, cur, conn)
     update_squad_and_title(conn, cur)
     conn.close()
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    minutes, seconds = divmod(execution_time, 60)
+    print(f"Script executed in {int(minutes)} minutes {int(seconds)} seconds! Let's go drink some beer :)")
 
 
 if __name__ == '__main__':
